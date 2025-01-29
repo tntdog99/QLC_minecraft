efcc280de75d3a5b33ae1d701ec6a15d8e7dc878
tntdog99 <86092163+tntdog99@users.noreply.github.com>
2025-01-29 12:27:06 -0700

    Create cdsd.lua

---
+++ cdsd.lua
@@ -0,0 +1,34 @@
+rednet.open("top")
+local function overworld()
+    while true do
+    sleep(0.05)
+        if fs.exists("cdsdtemp1") then
+        rednet.send(19, "_", "drs")
+        fs.delete("cdsdtemp1")
+        _, msg = rednet.receive("drsb")
+        local filewrite = fs.open("cdsdtemp2", "w")
+        filewrite.write(msg)
+        filewrite.close()
+        end
+    end
+end
+local function other()
+while true do
+sleep(0.05)
+if rednet.receive("drs") then
+local file2 = fs.open("cdsdtemp1", "w")
+file2.close()
+while true do
+sleep(0.05)
+if fs.getSize("cdsdtemp2") == 0 then
+else
+local fileread = fs.open("cdsdtemp2", "r")
+local msgb = fileread.readAll()
+rednet.send(11, msgb, "drs")
+break
+end
+end
+end
+end
+end
+parallel.waitForAll(other,overworld)
