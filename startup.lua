64039106bbdc32818fd8c9930247ba71c941bc42
tntdog99 <86092163+tntdog99@users.noreply.github.com>
2025-01-29 13:38:55 -0700

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -1,4 +1,4 @@
-shell.run("bg", "cdsr_base")
-shell.run("bg", "cdsr_fort")
-shell.run("bg", "cdsd")
-shell.run("bg", "updater")
+shell.run("bg", "cdsr_base.lua")
+shell.run("bg", "cdsr_fort.lua")
+shell.run("bg", "cdsd.lua")
+shell.run("bg", "updater.lua")
