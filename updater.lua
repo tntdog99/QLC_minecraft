48595e176d0de3ccc8a6952b4e7df8ec8362d056
tntdog99 <86092163+tntdog99@users.noreply.github.com>
2025-01-29 13:04:19 -0700

    Update updater.lua

--- updater.lua
+++ updater.lua
@@ -1,8 +1,8 @@
 
 shell.run("rm", "*")
-wget https://raw.githubusercontent.com/tntdog99/QLC_minecraft/new/main/startup.lua
-wget https://raw.githubusercontent.com/tntdog99/QLC_minecraft/new/main/cdsr_fort.lua
-wget https://raw.githubusercontent.com/tntdog99/QLC_minecraft/new/main/cdsr_base.lua
-wget https://raw.githubusercontent.com/tntdog99/QLC_minecraft/new/main/cdsd.lua
-wget https://raw.githubusercontent.com/tntdog99/QLC_minecraft/new/main/updater.lua
+shell.run("wget", "https://raw.githubusercontent.com/tntdog99/QLC_minecraft/new/main/startup.lua")
+shell.run("wget", "https://raw.githubusercontent.com/tntdog99/QLC_minecraft/new/main/cdsr_fort.lua")
+shell.run("wget", "https://raw.githubusercontent.com/tntdog99/QLC_minecraft/new/main/cdsr_base.lua")
+shell.run("wget", "https://raw.githubusercontent.com/tntdog99/QLC_minecraft/new/main/cdsd.lua")
+shell.run("wget", "https://raw.githubusercontent.com/tntdog99/QLC_minecraft/new/main/updater.lua")
 sleep(3600)
