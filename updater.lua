158cd608ede41d54606c074efb47a71f7f0ea66b
tntdog99 <86092163+tntdog99@users.noreply.github.com>
2025-01-29 12:42:42 -0700

    Create updater.lua

---
+++ updater.lua
@@ -0,0 +1,6 @@
+sleep(3600)
+wget run https://raw.githubusercontent.com/tntdog99/QLC_minecraft/new/main/startup.lua
+wget run https://raw.githubusercontent.com/tntdog99/QLC_minecraft/new/main/cdsr_fort.lua
+wget run https://raw.githubusercontent.com/tntdog99/QLC_minecraft/new/main/cdsr_base.lua
+wget run https://raw.githubusercontent.com/tntdog99/QLC_minecraft/new/main/cdsd.lua
+wget run https://raw.githubusercontent.com/tntdog99/QLC_minecraft/new/main/updater.lua
