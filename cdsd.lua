804f2b1a789502080345931aaaa2f0d2c83f5062
tntdog99 <86092163+tntdog99@users.noreply.github.com>
2025-01-30 14:57:23 -0700

    Update cdsd.lua

--- cdsd.lua
+++ cdsd.lua
@@ -1,33 +1,32 @@
 rednet.open("top")
 local filein = fs.openfs.open("cdsdtemp2", "w") overworld()
-    while true do
-    sleep(0.05)
-        if fs.exists("cdsdtemp1") then
-        rednet.send(19, "_", "drs")
-        fs.delete("cdsdtemp1")
-        _, msg = rednet.receive("drsb")
-        local filewrite = fs.open("cdsdtemp2", "w")
-        filewrite.write(msg)
-        end
+while true do
+sleep(0.05)
+    if fs.exists("cdsdtemp1") then
+    rednet.send(19, "_", "drs")
+    fs.delete("cdsdtemp1")
+    _, msg = rednet.receive("drsb")
+    local filewrite = fs.open("cdsdtemp2", "w")
+    filewrite.write(msg)
     end
 end
 local function other()
-while true do
-sleep(0.05)
-if rednet.receive("drs") then
-local file2 = fs.open("cdsdtemp1", "w")
-file2.close()
-while true do
-sleep(0.05)
-if fs.getSize("cdsdtemp2") == 0 then
-else
-local fileread = fs.open("cdsdtemp2", "r")
-local msgb = fileread.readAll()
-rednet.send(11, msgb, "drs")
-break
-end
-end
-end
-end
+    while true do
+    sleep(0.05)
+        if rednet.receive("drs") then
+        local file2 = fs.open("cdsdtemp1", "w")
+        file2.close()
+            while true do
+            sleep(0.05)
+                if fs.getSize("cdsdtemp2") == 0 then
+                else
+                local fileread = fs.open("cdsdtemp2", "r")
+                local msgb = fileread.readAll()
+                rednet.send(11, msgb, "drs")
+                break
+                end
+            end
+        end
+    end
 end
 parallel.waitForAll(other,overworld)
