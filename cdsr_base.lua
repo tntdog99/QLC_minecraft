e414a59917361fcaa36f9d15b0168fb3cf33a964
tntdog99 <86092163+tntdog99@users.noreply.github.com>
2025-01-29 18:12:09 -0700

    Update cdsr_base.lua

--- cdsr_base.lua
+++ cdsr_base.lua
@@ -36,7 +36,7 @@ print("runing")
         
                     rednet.send(SNID, msg, "item_network")
                     _, that2 = rednet.receive("item_network")
-                    fileSNWrite = fs.open("cdsrbase2", "w")
+                    local fileSNWrite = fs.open("cdsrbase2", "w")
                     fileSNWrite.write(textutils.serialise(that2))
                 end
             end
