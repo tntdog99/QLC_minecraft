153eecc0a890c9a04cdb373270a066c28a8df393
tntdog99 <86092163+tntdog99@users.noreply.github.com>
2025-01-30 18:51:29 -0700

    Update filenet.lua

--- filenet.lua
+++ filenet.lua
@@ -1,12 +1,42 @@
 function filenet.send(text, protocol)
 	if text and protocol then
-		local "file..protocol" = fs.open("filenet/temp."..protocol, "w")
-
-
+		local file = fs.open("/filenet/temp."..protocol, "w")
+			if textutils.serialize(text) then
+				file.write(textutils.serialize(text))
+				file.close()
+			else
+				file.write(text)
+				file.close()
+			end
 	elseif text then
 		error("No protocol provided")
 	elseif protocol then
 		error("No text provided")
 	end
 end
-  
+
+
+
+
+function filenet.receive(protocol)
+	if protocol then
+		local filerec = fs.open("/filenet/temp."..protocol, "w")
+		local filerec = fs.open("/filenet/temp."..protocol, "r")
+		while true do
+			sleep(0.05)
+			if fs.getSize("/filenet/temp."..protocol) == 0 then
+			else
+				local text2 = filerec.readAll()
+				if textutils.unserialize(text2) then
+					fs.delete("/filenet/temp."..protocol)
+				return textutils.unserialize(text2)
+			else
+					fs.delete("/filenet/temp."..protocol)
+				return text2
+				end
+			end	
+		end
+	else
+	error("No protocol")
+	end
+end
