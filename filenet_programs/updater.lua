23d41d67acc870a6e9b6a2887d3c26b3294ef472
tntdog99 <86092163+tntdog99@users.noreply.github.com>
2025-01-31 16:58:29 -0700

    Update updater.lua

--- filenet_programs/updater.lua
+++ filenet_programs/updater.lua
@@ -11,5 +11,5 @@ shell.run("wget http://raw.githubusercontent.com/TNTDOG99/QLC_minecraft/main/fil
 shell.run("wget http://raw.githubusercontent.com/TNTDOG99/QLC_minecraft/main/filenet_programs/cdsr_base.lua")
 shell.run("wget http://raw.githubusercontent.com/TNTDOG99/QLC_minecraft/main/filenet_programs/cdsd.lua")
 shell.run("wget http://raw.githubusercontent.com/TNTDOG99/QLC_minecraft/main/filenet_programs/updater.lua")
-shell.run("wget https://raw.githubusercontent.com/TNTDOG99/QLC_minecraft/main/filenetapi/filenet.lua")
+shell.run("wget https://raw.githubusercontent.com/TNTDOG99/QLC_minecraft/main/filenetapi/.filenet.lua")
 os.reboot()
