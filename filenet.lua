8122fcdc52ac4115c6a257228d23cd0ffac65476
tntdog99 <86092163+tntdog99@users.noreply.github.com>
2025-01-30 17:50:40 -0700

    Update and rename filenet.api to filenet.lua

---
+++ filenet.lua
@@ -0,0 +1,12 @@
+function filenet.send(text, protocol)
+	if text and protocol then
+		local file..protocol = fs.open("filenet/temp."..protocol, "w")
+
+
+	elseif text then
+		error("No protocol provided")
+	elseif protocol then
+		error("No text provided")
+	end
+end
+  
