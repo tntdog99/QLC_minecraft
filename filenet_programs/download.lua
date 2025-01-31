7ca9737784a95590c10b7e6c3a16e27e721826a2
tntdog99 <86092163+tntdog99@users.noreply.github.com>
2025-01-30 22:45:35 -0700

    Update download.lua

--- filenet_programs/download.lua
+++ filenet_programs/download.lua
@@ -1,6 +1,6 @@
 shell.run("wget http://raw.githubusercontent.com/TNTDOG99/QLC_minecraft/main/filenet_programs/startup.lua")
 shell.run("wget http://raw.githubusercontent.com/TNTDOG99/QLC_minecraft/main/filenet_programs/cdsr_fort.lua")
-shell.run("wget http://raw.githubusercontent.com/TNTDOG99/QLC_minecraft/main/filenet_programs/cdsrbase.lua")
+shell.run("wget http://raw.githubusercontent.com/TNTDOG99/QLC_minecraft/main/filenet_programs/cdsr_base.lua")
 shell.run("wget http://raw.githubusercontent.com/TNTDOG99/QLC_minecraft/main/filenet_programs/cdsd.lua")
 shell.run("wget http://raw.githubusercontent.com/TNTDOG99/QLC_minecraft/main/filenet_programs/updater.lua")
 sleep(5)
