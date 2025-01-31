1dbf3160fbda351bc2aae93fb9be246dc6ce16e3
tntdog99 <86092163+tntdog99@users.noreply.github.com>
2025-01-30 21:09:03 -0700

    Update  cdsd.lua

--- filenet_programs/ cdsd.lua
+++ filenet_programs/ cdsd.lua
@@ -1,14 +1,13 @@
+local filenet = require("filenet")
 rednet.open("top")
 local filein = fs.open("cdsdtemp2", "w")
 local function overworld()
     while true do
     sleep(0.05)
-        if fs.exists("cdsdtemp1") then
+        if filenet.receive("cdsd1") then
         rednet.send(19, "_", "drs")
-        fs.delete("cdsdtemp1")
         _, msg = rednet.receive("drsb")
-        local filewrite = fs.open("cdsdtemp2", "w")
-        filewrite.write(msg)
+filenet.send(msg,"cdsd2")
         end
     end
 end
@@ -16,18 +15,9 @@ local function other()
     while true do
     sleep(0.05)
         if rednet.receive("drs") then
-        local file2 = fs.open("cdsdtemp1", "w")
-        file2.close()
-            while true do
-            sleep(0.05)
-                if fs.getSize("cdsdtemp2") == 0 then
-                else
-                local fileread = fs.open("cdsdtemp2", "r")
-                local msgb = fileread.readAll()
+filenet.send("true", "cdsd1")
+local msgb = filenet.receive("cdsd2")
                 rednet.send(11, msgb, "drs")
-                break
-                end
-            end
         end
     end
 end
