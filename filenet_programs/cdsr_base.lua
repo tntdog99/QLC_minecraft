68c7c1021c926f9eaf91bf101147b01ec2b0feff
tntdog99 <86092163+tntdog99@users.noreply.github.com>
2025-02-01 15:52:39 -0700

    update

--- filenet_programs/cdsr_base.lua
+++ filenet_programs/cdsr_base.lua
@@ -3,36 +3,31 @@ rednet.open("top")
 local SNID = 30
 local table = {}
 local function overworld()
-print("runing")
+    print("runing")
     while true do
-    sleep(0.05)
-    local this3 = filenet.receive("cdsrbase1")
-            id = this3.id
-            msg = this3.msg
-            print(msg.type)
-            if this3 then
-                if msg.type == "list" then
-                     rednet.send(SNID, { type = "list" }, "item_network")
-                    _, that2 = rednet.receive("item_network", 5)
-                    filenet.send(that2,"cdsrbase2")
-                elseif msg.type == "request" then 
-        
-                    rednet.send(SNID, msg, "item_network")
-                    _, that2 = rednet.receive("item_network")
-                    filenet.send(that2,"cdsrbase1")
-                end
-            end
+        sleep(0.05)
+        local this3 = filenet.receive("cdsrbase1")
+        id = this3.id
+        msg = this3.msg
+        print(msg.type)
+        if msg.type == "list" then
+            rednet.send(SNID, { type = "list" }, "item_network")
+            _, that2 = rednet.receive("item_network")
+            filenet.send(that2,"cdsrbase2")
+        elseif msg.type == "request" then 
+            rednet.send(SNID, msg, "item_network")
+            _, that2 = rednet.receive("item_network")
+            filenet.send(that2,"cdsrbase1")
         end
     end
+end
 local function other()
     while true do
-        this, that = rednet.receive("twoitem_network", 5)
+        this, that = rednet.receive("twoitem_network")
         table = {id = this, msg = that}
-        sleep(0.05)
         if this then
             print("msg got")
             filenet.send(table, "cdsrbase1")
-            sleep(0.05)
             local that3 = filenet.receive("cdsrbase2")
             rednet.send(this, that3, "twoitem_network")
         end
--- filenet_programs/cdsr_fort.lua
+++ filenet_programs/cdsr_fort.lua
@@ -7,32 +7,27 @@ local function overworld()
     while true do
         sleep(0.05)
         local this3 = filenet.receive("cdsrfort1")
-            id = this3.id
-            msg = this3.msg
-            print(msg.type)
-            if this3 then
-                if msg.type == "list" then
-                     rednet.send(SNID, { type = "list" }, "item_network")
-                    _, that2 = rednet.receive("item_network", 5)
-                    filenet.send(that2,"cdsrfort2")
-                elseif msg.type == "request" then 
-        
-                    rednet.send(SNID, msg, "item_network")
-                    _, that2 = rednet.receive("item_network")
-                    filenet.send(that2,"cdsrfort1")
-                end
-            end
+        id = this3.id
+        msg = this3.msg
+        print(msg.type)
+        if msg.type == "list" then
+            rednet.send(SNID, { type = "list" }, "item_network")
+            _, that2 = rednet.receive("item_network")
+            filenet.send(that2,"cdsrfort2")
+        elseif msg.type == "request" then 
+            rednet.send(SNID, msg, "item_network")
+            _, that2 = rednet.receive("item_network")
+            filenet.send(that2,"cdsrfort1")
         end
     end
+end
 local function other()
     while true do
-        this, that = rednet.receive("cdsr_item_network", 5)
+        this, that = rednet.receive("cdsr_item_network")
         table = {id = this, msg = that}
-        sleep(0.05)
         if this then
             print("msg got")
             filenet.send(table, "cdsrfort1")
-            sleep(0.05)
             local that3 = filenet.receive("cdsrfort2")
             rednet.send(this, that3, "cdsr_item_network")
         end
