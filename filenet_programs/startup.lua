27dd9a1c7c2fcd7508b8b2c09ae84642e7310b5b
tntdog99 <86092163+tntdog99@users.noreply.github.com>
2025-01-31 23:23:41 -0700

    Update startup.lua

--- filenet_programs/startup.lua
+++ filenet_programs/startup.lua
@@ -2,3 +2,6 @@ shell.run("bg", "cdsr_base.lua")
 shell.run("bg", "cdsr_fort.lua")
 shell.run("bg", "cdsd.lua")
 shell.run("bg", "updater.lua")
+if fs.exists("filenet") then
+  _G.filenet = require("filenet")
+end
