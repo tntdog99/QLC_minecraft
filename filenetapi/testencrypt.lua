d0d4321e664e58af360a396d7045c7066eafafe6
tntdog99 <86092163+tntdog99@users.noreply.github.com>
2025-02-02 17:31:29 -0700

    update

--- filenetapi/testencrypt.lua
+++ filenetapi/testencrypt.lua
@@ -16,6 +16,48 @@ function unshift(text, shift_amount)
     return table.concat(result)
 end
 
+function custom_encrypt(text, key)
+    local text_len = #text
+    local key_len = #key
+    local interval = math.floor(text_len / key_len)
+    local result = {}
+
+    local key_index = 1
+    for i = 1, text_len do
+        table.insert(result, string.sub(text, i, i))
+        if i % interval == 0 and key_index <= key_len then
+            table.insert(result, string.sub(key, key_index, key_index))
+            key_index = key_index + 1
+        end
+    end
+
+    -- Append remaining key characters if any
+    while key_index <= key_len do
+        table.insert(result, string.sub(key, key_index, key_index))
+        key_index = key_index + 1
+    end
+
+    return table.concat(result)
+end
+
+function custom_decrypt(encrypted_text, key)
+    local text_len = #encrypted_text
+    local key_len = #key
+    local interval = math.floor((text_len - key_len) / key_len)
+    local result = {}
+
+    local key_index = 1
+    for i = 1, text_len do
+        if key_index <= key_len and i == (key_index - 1) * interval + key_index then
+            key_index = key_index + 1
+        else
+            table.insert(result, string.sub(encrypted_text, i, i))
+        end
+    end
+
+    return table.concat(result)
+end
+
 function xorEncrypt(data, key)
     local result = {}
     for i = 1, #data do
